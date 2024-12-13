836ec0137ca535435ad92e0c1bfa7dc9eb830d8a
Niraj-codee <nirajkumar778087@gmail.com>
2024-12-13 22:33:55 +0530

    Add files via upload

---
+++ Program.java
@@ -0,0 +1,87 @@
+/* Output, Variables Data Type, Input in java */
+
+
+
+// public class Program {
+//     public static void main(String[] args) {
+//         System.out.println("Hello World");
+//     }
+// }
+
+
+/* Print the Pattern */
+
+
+// public class Program {
+//     public static void main(String[] args) {
+//         System.out.println("*");
+//         System.out.println("**");
+//         System.out.println("***");
+//         System.out.println("****");
+//     }
+    
+// }
+
+/* Variables in java */
+
+
+// public class Program {
+//     public static void main(String[] args) {
+//         int a = 10;
+//         int b = 5;
+//         int sum = a+b;
+//         System.out.println(sum);
+//     }
+// }
+
+
+/* Quiz  */
+
+
+// public class Program {
+
+//     public static void main(String[] args) {
+//         int a = 10;
+//         int b = 5;
+//         System.out.println(a*b/a-b);
+//         System.out.println((a*b)/(a-b));
+//     }
+// }
+
+
+/* Input in java */
+
+
+// import java.util.*;
+// public class Program {
+
+// import java.util.Scanner;
+
+//     public static void main(String[] args) {
+//         Scanner Sc = new Scanner(System.in);
+//         String name = Sc.nextLine();
+
+//         System.out.println(name);
+//     }
+// }
+
+/* Take 2 variables a abd b abc print there sum */
+
+
+import java.util.Scanner;
+
+public class Program {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a : ");
+        int a = sc.nextInt();
+        System.out.print("Emter b : ");
+        int b = sc.nextInt();
+        int sum = a+b;
+        System.out.println("Total number :"+sum);
+    }
+}
+
+/*  */
+
